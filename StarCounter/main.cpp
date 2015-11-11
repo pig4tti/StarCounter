dffd23ffcc76fe296ca14777147bc5e24a47bbe4
Rodrigo Pigatti Marroquim <marroquim@apple.com>
2015-11-11 00:07:29 -0300

    Initial Commit

---
+++ StarCounter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StarCounter
+//
+//  Created by Rodrigo Pigatti Marroquim on 11/11/15.
+//  Copyright (c) 2015 Rodrigo Pigatti Marroquim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
